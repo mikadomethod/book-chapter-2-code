8166895d2b443edde49f01521b817ee9c7241c6e
Daniel Brolund <daniel.brolund@agical.com>
2013-08-13 19:44:52 +0200

    Optimized imports

--- src/main/java/org/mikadomethod/app/db/FileDB.java
+++ src/main/java/org/mikadomethod/app/db/FileDB.java
@@ -2,9 +2,7 @@ package org.mikadomethod.app.db;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
