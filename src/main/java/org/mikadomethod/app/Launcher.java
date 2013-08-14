692fec4e7706e9599c43a783cbc7a6ab508d9e94
Daniel Brolund <daniel.brolund@agical.com>
2013-08-15 00:53:08 +0200

    Final version

--- src/main/java/org/mikadomethod/app/Launcher.java
+++ src/main/java/org/mikadomethod/app/Launcher.java
@@ -5,16 +5,13 @@ import org.mikadomethod.app.ui.App;
 import org.mikadomethod.app.ui.ApplicationException;
 
 public class Launcher {
-
 	public static void main(String[] argv) {
-			try {
-				App.setStorageFile(argv[0]);
-				App app = new App();
-				app.launch(new FileDB(App.getStorageFile()));
-			} catch (ApplicationException e) {
-				System.err.println("Could not start application");
-				e.printStackTrace();
-			}
+		try {
+			App app = new App();
+			app.launch(new FileDB(argv[0]));
+		} catch (ApplicationException e) {
+			System.err.println("Could not start application");
+			e.printStackTrace();
+		}
 	}
- 
 }
--- src/main/java/org/mikadomethod/app/ui/App.java
+++ src/main/java/org/mikadomethod/app/ui/App.java
@@ -3,21 +3,10 @@ package org.mikadomethod.app.ui;
 import org.mikadomethod.app.db.Database;
 
 public class App {
-
 	private UI ui;
-	private static String storePath;
 
 	public void launch(Database database) throws ApplicationException {
 		ui = new UI(database);
 		ui.showLogin();
 	}
-
-	public static String getStorageFile() {
-		return storePath;
-	}
-
-	public static void setStorageFile(String storePath) {
-		App.storePath = storePath;
-	}
-
 }
