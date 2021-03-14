45a6978b8f1e031ed3f124a61edb7eb05b80dd8b
gangov <gangov1@gmail.com>
2021-03-14 19:21:17 +0200

    fixes Error: Could not find or load main class org.mikadomethod.app.Launcher error message

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -cp classes org.mikadomethod.app.Launcher
+javac -cp src/main/java src/main/java/org/mikadomethod/app/Launcher.java
+java -cp src/main/java/ org.mikadomethod.app.Launcher
